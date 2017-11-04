1b0cf3ed7b3bcb86cf3b57613e9977a2aebd095e
phantom5552000 <nakagawa.isamu@gmail.com>
2017-11-04 09:23:16 +0900

    ---

--- src/renderer/ts/interfaces/favorite.interface.ts
+++ src/renderer/ts/interfaces/favorite.interface.ts
@@ -2,10 +2,11 @@ import {IProgram} from '../interfaces/program.interface';
 import {IStation, IRegion} from '../interfaces/station.interface';
 
 export interface IFavorite{
-    name: string;
-    lastUpdate:Date;
-    size:string;
-    fullName:string;
-    station: IStation;
+    //name: string;
+    //lastUpdate:Date;
+    //size:string;
+    //fullName:string;
+    station_id: string;
+    station_name: string;
     program: IProgram;
 }
